727348ba1e203dade636a12437e37cf7fa0c4606
Vinit <vinits1014@gmail.com>
2024-05-25 15:52:10 +0530

    Done with majority of work

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   return (
     <>
-      <Navbar />
+      <Navbar email={undefined} handleFun={undefined} />
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <div className="max-w-xl text-center">
           <h2 className="text-4xl font-bold mb-8">Welcome to SecureShare</h2>
