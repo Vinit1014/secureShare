9270c45ffdb6d021ab5bf8aeadd09ac6d461d435
Vinit <vinits1014@gmail.com>
2024-05-25 15:08:39 +0530

    Done with majority of work

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { authOptions } from "@/lib/auth";
-// import nextAuth from "next-auth";
-import NextAuth from "next-auth/next";
-
-const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST}
